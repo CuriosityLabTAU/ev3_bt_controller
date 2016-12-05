0848b1ab3d8d495c5109300f82d67d9167d73a36
Goren Gordon <goren@gorengordon.dom>
2016-12-05 13:45:27 +0200

    added memory in neural networs

--- neuronets.py
+++ neuronets.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from random import shuffle
 
 
 class NN:
@@ -17,6 +18,12 @@ class NN:
         self.output1_index = output1_index
         self.viable = viable
         self.i_mul = i_mul
+
+        self.z_memory = []
+        self.memory_size = 10
+        self.memory_index = 0
+
+        self.image_patch = 1
 
     def sig(z):
         h = np.tanh(z)
@@ -108,46 +115,65 @@ class NN:
                 self.Wa2 = np.delete(self.Wa2, prune_index + 1, 1)
 
 
-    def learn(self, z):
+    def organize_training_set(self, z_memory):
+        # receives z_memory
+        training_set = []
+        for m in range(0, len(z_memory)):
+            for p in range(0, z_memory[m][-1].flatten().shape[0]):      # only for first level (higher, are patches)
+                t_set = z_memory[m][0:-2]                    # all the non-image states
+                t_set.append(z_memory[m][-2].flatten()[p])   # the pixel p from the last-minus-one image
+                t_set.append(z_memory[m][-1].flatten()[p])   # the pixel p from the last image
+                training_set.append(t_set)
+        shuffle(training_set)
+        return training_set
 
-        x11 = np.asarray(z[self.input1_index])
-        x12 = np.asarray(z[self.input2_index])
-        d11 = np.asarray(z[self.output1_index])
-        x11 = x11.flatten()
-        x12 = x12.flatten()
-        d11 = d11.flatten()
-        size_x11 = x11.shape[0]
-        size_x12 = x12.shape[0]
-        size_d11 = d11.shape[0]
-        N_batch = np.amax([size_x11, size_x12, size_d11])
-        if size_x11 == 1:
-            x11 = np.concatenate(np.ones((N_batch, 1)) * x11[0], axis=0)
-        if size_x12 == 1:
-            x12 = np.concatenate(np.ones((N_batch, 1)) * x12[0], axis=0)
-        if size_d11 == 1:
-            #d11 = np.ones((N_batch, 1)) * d11[0]
-            d11 = np.concatenate(np.ones((N_batch, 1)) * d11[0], axis=0)
-        eta_batch = self.eta/N_batch
-        D1 = 0
-        D2 = 0
-        for i in range(0, N_batch):
-            x1 = [x11[i], x12[i]]
-            d12 = d11[i]
-            xa, s1, za, s2, y1 = self.forProp(x1)
-            e2 = d12-y1
-            sigtag2 = NN.sigtag(s2)
-            d2 = np.multiply(e2, sigtag2)
-            D2 += np.outer(-d2, za.T)
-            sigtag1 = NN.sigtag(s1)
-            p = self.Wa2.shape[1]-1
-            W2 = self.Wa2[:, 1:p+1]
-            e1 = np.dot(W2.T, d2)
-            d1 = e1*sigtag1
-            D1 += np.outer(-d1, xa.T)
-        self.Wa2 -= eta_batch * D2 + self.pruning_rate * np.sign(self.Wa2)
-        self.Wa1 -= eta_batch * D1 + self.pruning_rate * np.sign(self.Wa1)
-        J = self.cost(d12, y1)
-        self.removeNode()
+    def learn(self, z):
+        self.z_memory[self.memory_index % self.memory_size] = z         # update memory buffer
+        self.memory_index += 1
+
+        training_set = self.organize_training_set(self.z_memory)        # generate training_set from memory
+
+        for t in training_set:                                          # go over all training set
+
+            x11 = np.asarray(t[self.input1_index])
+            x12 = np.asarray(t[self.input2_index])
+            d11 = np.asarray(t[self.output1_index])
+
+            x11 = x11.flatten()
+            x12 = x12.flatten()
+            d11 = d11.flatten()
+            size_x11 = x11.shape[0]
+            size_x12 = x12.shape[0]
+            size_d11 = d11.shape[0]
+            N_batch = np.amax([size_x11, size_x12, size_d11])
+            if size_x11 == 1:
+                x11 = np.concatenate(np.ones((N_batch, 1)) * x11[0], axis=0)
+            if size_x12 == 1:
+                x12 = np.concatenate(np.ones((N_batch, 1)) * x12[0], axis=0)
+            if size_d11 == 1:
+                #d11 = np.ones((N_batch, 1)) * d11[0]
+                d11 = np.concatenate(np.ones((N_batch, 1)) * d11[0], axis=0)
+            eta_batch = self.eta/N_batch
+            D1 = 0
+            D2 = 0
+            for i in range(0, N_batch):
+                x1 = [x11[i], x12[i]]
+                d12 = d11[i]
+                xa, s1, za, s2, y1 = self.forProp(x1)
+                e2 = d12-y1
+                sigtag2 = NN.sigtag(s2)
+                d2 = np.multiply(e2, sigtag2)
+                D2 += np.outer(-d2, za.T)
+                sigtag1 = NN.sigtag(s1)
+                p = self.Wa2.shape[1]-1
+                W2 = self.Wa2[:, 1:p+1]
+                e1 = np.dot(W2.T, d2)
+                d1 = e1*sigtag1
+                D1 += np.outer(-d1, xa.T)
+            self.Wa2 -= eta_batch * D2 + self.pruning_rate * np.sign(self.Wa2)
+            self.Wa1 -= eta_batch * D1 + self.pruning_rate * np.sign(self.Wa1)
+            J = self.cost(d12, y1)
+            self.removeNode()
         return J
 
 
